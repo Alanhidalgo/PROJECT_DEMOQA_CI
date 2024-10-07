e16570d378baa3ff4cb656bf3f92951175c64921
Alan Hidalgo <Alan827hidalgo@gmail.com>
2024-10-07 12:28:03 -0300

    Ejercicio completo de formulario y enviar

--- tests/form/test_form_pratice.py
+++ tests/form/test_form_pratice.py
@@ -8,16 +8,69 @@ from selenium.webdriver.support import expected_conditions as EC
 class TestPracticeForm:
     driver: WebDriver
 
-    def test_form_enable(self):
+    def test_form_enable(self, clear_fields):
         # Validar que los campos del formulario están vacíos y habilitados
         form_fields = {
-            "name": self.driver.find_element(By.ID, "firstName"),
-            "lastname": self.driver.find_element(By.ID, "lastName"),
+            "first_name": self.driver.find_element(By.ID, "firstName"),
+            "last_name": self.driver.find_element(By.ID, "lastName"),
             "email": self.driver.find_element(By.ID, "userEmail"),
-            "gender": 
+            "gender_male": self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-1']"),
+            "gender_female": self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-2']"),
+            "gender_other": self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-3']"),
             "mobile": self.driver.find_element(By.ID, "userNumber"),
-            "date_birth": 
-            "subject": self.driver.find_element(By.XPATH, "//*[@id="subjectsContainer"]/div/div[1]"),
-            "hobbies": 
-            "address": self.driver.find_element(By.ID, "currentAddress")   
+            "date_of_birth": self.driver.find_element(By.ID, "dateOfBirthInput"),
+            "subject": self.driver.find_element(By.ID, "subjectsInput"),
+            "hobby_sports": self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-1']"),
+            "hobby_reading": self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-2']"),
+            "hobby_music": self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-3']"),
+            "picture_upload": self.driver.find_element(By.ID, "uploadPicture"),
+            "current_address": self.driver.find_element(By.ID, "currentAddress"),
+            "state_city": self.driver.find_element(By.ID, "stateCity-wrapper")
         }
+
+        #time.sleep(5)
+
+        clear_fields(form_fields.values())
+
+        #validamos que los campos esten habilitados
+        for field_name, field_element in form_fields.items():
+            assert field_element.is_enabled(), f"El campo '{field_name}' no esta habilitado"
+
+        # validar boton submit
+        submit_button = self.driver.find_element(By.ID, "submit")
+        assert submit_button.is_displayed() and submit_button.is_enabled(), "El boton submit no esta disponible."
+
+    def test_student_registration_form(self, ramdom_users):
+        # Seleccionamos el usuario a enviar
+        user = ramdom_users["users"][2]
+
+        form_fields = {
+            "first_name": self.driver.find_element(By.ID, "firstName"),
+            "last_name": self.driver.find_element(By.ID, "lastName"),
+            "email": self.driver.find_element(By.ID, "userEmail"),
+            "gender_male": self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-1']"),
+            "gender_female": self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-2']"),
+            "gender_other": self.driver.find_element(By.CSS_SELECTOR, "label[for='gender-radio-3']"),
+            "mobile": self.driver.find_element(By.ID, "userNumber"),
+            "date_of_birth": self.driver.find_element(By.ID, "dateOfBirthInput"),
+            "subject": self.driver.find_element(By.ID, "subjectsInput"),
+            "hobby_sports": self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-1']"),
+            "hobby_reading": self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-2']"),
+            "hobby_music": self.driver.find_element(By.CSS_SELECTOR, "label[for='hobbies-checkbox-3']"),
+            "picture_upload": self.driver.find_element(By.ID, "uploadPicture"),
+            "current_address": self.driver.find_element(By.ID, "currentAddress"),
+            "state_city": self.driver.find_element(By.ID, "stateCity-wrapper")
+        }
+
+        #Llenar los campos del form
+        form_fields["first_name"].send_keys(user["username"])
+        form_fields["last_name"].send_keys(user["lastname"])
+        form_fields["email"].send_keys(user["email"])
+        form_fields["gender_male"].click()
+        form_fields["mobile"].send_keys(user["phone_number"])
+
+        #boton de enviar
+        submit_button = self.driver.find_element(By.ID, "submit")
+        time.sleep(10)
+        submit_button.click()
+        time.sleep(10)
---
+++ tests/form/test_leblox.py
@@ -0,0 +1,23 @@
+Instrucciones
+Escribe una secuencia lógica o pseudocódigo (en cualquier lenguaje que prefieras) para un script automatizado que valide una lista de resultados 
+de pruebas en un sistema de pruebas automatizado.
+
+Escenario:
+Tienes una lista de resultados de pruebas, donde cada prueba tiene un estado asociado que puede ser "PASSED", "FAILED" o "SKIPPED". 
+Necesitas implementar una lógica que realice las siguientes validaciones:
+
+Si todas las pruebas están en estado "PASSED", imprime "Todos los tests pasaron exitosamente."
+Si todas las pruebas están en estado "FAILED", imprime "Todos los tests fallaron."
+Si todas las pruebas están en estado "SKIPPED", imprime "Todos los tests fueron saltados."
+Si hay una mezcla de estados "PASSED" y "FAILED", imprime "Algunos tests pasaron y algunos fallaron."
+Si hay una mezcla de estados "SKIPPED" y cualquier otro estado, imprime "Algunos tests fueron saltados."
+
+Requisitos:
+La función debe recibir una lista de resultados de pruebas.
+No se permite probar el código; debes confiar en tu traza mental y lógica.
+Debes estructurar tu lógica de forma clara y eficiente, y puede estar en pseudocódigo o en un lenguaje de programación de tu elección.
+Tienes 30 minutos para realizar la prueba.
+
+Consideraciones:
+Presta atención a los diferentes estados de las pruebas y asegúrate de cubrir todos los posibles escenarios.
+Tu solución debe ser sencilla y clara.
