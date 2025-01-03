760e02e72770badf5c31d90ac6f90af0a50186d4
Alan Hidalgo <Alan827hidalgo@gmail.com>
2025-01-03 16:13:37 -0300

    Modificación archivo conftest - fixture navigate_to_practice_form

--- tests/conftest.py
+++ tests/conftest.py
@@ -61,7 +61,7 @@ def navigate_to_webtables(request):
     # Usa el driver que está en request.cls.driver
     request.cls.driver.get("https://demoqa.com/webtables")  # Navegar a la página web
 
-# fixture para navegar a demoqa, webbtables
+# fixture para navegar a demoqa, practice form
 @pytest.fixture
 def navigate_to_practice_form(request):
     # Usa el driver que está en request.cls.driver
